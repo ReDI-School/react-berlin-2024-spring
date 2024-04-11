5f48eaf63057d0cc8387f7243067fa6b029ebde1
kgrhartlage <kai.grosse.hartlage@gmail.com>
2024-04-11 16:41:35 +0200

    Add exercises

--- src/views/RestaurantView.jsx
+++ src/views/RestaurantView.jsx
@@ -5,7 +5,21 @@ import risottoImage from '../assets/risotto.jpg';
 import { useState } from 'react';
 import Cart from '../components/Cart.jsx';
 import MenuItem from '../components/MenuItem.jsx';
-import Lesson8Exercise1 from '../exercises/lesson8/Lesson8Exercise1';
+import Lesson8 from '../exercises/lesson8/Lesson8.jsx';
+
+/**
+ * INSTRUCTIONS:
+ *
+ * 1. Refactor the cart so that it can store multiple items.
+ * 2. Add the ability to clear the entire cart.
+ * 3. Add the ability to remove individual items from the cart. Refactor the
+ * existing "Add To Cart" buttons for this purpose, do not add new buttons. The
+ * buttons should change their label and background color based on whether the
+ * menu is in the cart or not.
+ * 4. Add a message that notifies the user about having unlocked a 10% discount
+ * when the cart has 3 items. This message should only be visible when this
+ * condition is met.
+ */
 
 const RestaurantView = () => {
   const [selectedMenuItem, setSelectedMenuItem] = useState('empty');
@@ -16,7 +30,7 @@ const RestaurantView = () => {
 
   return (
     <>
-      <h1>REDI React Restaurant</h1>
+      <h1>ReDI React Restaurant</h1>
       <Cart selectedMenuItem={selectedMenuItem} />
       <div className="menu">
         <MenuItem
@@ -35,7 +49,7 @@ const RestaurantView = () => {
           onClick={onMenuItemClicked}
         />
       </div>
-      <Lesson8Exercise1 />
+      <Lesson8 />
     </>
   );
 };
