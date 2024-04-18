cb889be6c5770f665961fbb55d23ecc61720c536
Fábio Rodrigues <me@fabiorodrigu.es>
2024-04-18 17:45:09 +0200

    feat(Lesson10): added second exercise

---
+++ src/components/SearchField/SearchField.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import styles from "./SearchField.module.css";
+
+/**
+ * Exercise together
+ * Set the class .mouseEnterBorder with onMouseEnter
+ * Remove the class .mouseEnterBorder with onMouseLeave
+ * Set the class .focusedBorder with onFocus
+ * Remove the class .focusedBorder with onBlur
+ * Filter the list in the RestaurantView with onChange
+ */
+
+const SearchField = ({ filterDishes }) => {
+  const [searchClasses, setSearchClasses] = useState([styles.search]);
+
+  // USE ME IN THE INPUT FILED
+  const handleOnChange = (event) => {
+    filterDishes(event.target.value);
+  };
+
+  return (
+    <div>
+      <input className={searchClasses.join(" ")} type="text" />
+    </div>
+  );
+};
+
+export default SearchField;
--- src/views/RestaurantView.jsx
+++ src/views/RestaurantView.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import Cart from "../components/Cart/Cart.jsx";
 import MenuItem from "../components/MenuItem/MenuItem.jsx";
-import dishes from "../mocks/dishes.json";
+import rawDishes from "../mocks/dishes.json";
 
 import styles from "./RestaurantView.module.css";
 import NavBar from "../components/NavBar/NavBar.jsx";
+import SearchField from "../components/SearchField/SearchField.jsx";
 
 const RestaurantView = () => {
+  const [dishes, setDishes] = useState(rawDishes);
   const [selectedItems, setSelectedItems] = useState([]);
 
   const handleMenuClick = (name) => {
@@ -21,10 +23,16 @@ const RestaurantView = () => {
     setSelectedItems(nextItems);
   };
 
+  const handleDishesFilter = (query) => {
+    console.log(query);
+  };
+
   return (
     <>
       <NavBar>
         <h1>ReDI React Restaurant</h1>
+
+        <SearchField filterDishes={handleDishesFilter} />
 
         <Cart
           selectedItems={selectedItems}
