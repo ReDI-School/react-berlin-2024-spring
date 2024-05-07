957fa9498280264e9e8a7a1b7491f1dc5145a319
PuffCab <berlinkollner@gmail.com>
2024-05-07 16:04:49 +0200

    exercise added

---
+++ src/exercises/lesson13/Lesson13Exercise.jsx
@@ -0,0 +1,15 @@
+/**
+ *  INSTRUCTIONS:
+ *  Build a small app that renders the information coming from the API https://restcountries.com/v3.1/all.
+ * Build a component in which you should make a request to the API and render a list of elements.
+ *  Each element should represent every single country , in the shape of a card (you can reuse the style used for <MenuItem/>)
+ * Every Card should contain an <h3/> with the name of the country, and an <img/> with the flag.
+ * OPTONALLY: under the Flag, render the follwing information: Symbol of the currency (or currencies) of that country, and the capital (or capitals)
+ * */
+
+const Lesson13Exercise = () => {
+  const URL = "https://restcountries.com/v3.1/all";
+  return <div>Lesson13Exercise</div>;
+};
+
+export default Lesson13Exercise;
