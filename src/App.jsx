d9156faac99d0248f2642b6962e5983e30572aa3
Paul Johnson <Paul@PJOHNSOMAC-6366.digi.box>
2024-05-16 07:02:54 -0400

    homework solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import "./App.css";
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary.jsx";
-import DataFetch from "./examples/lesson13/DataFetch.jsx";
-import DataFetch2 from "./examples/lesson13/DataFetch2.jsx";
-import Lesson14Example from "./examples/lesson14/Lesson14Example.jsx";
-import Lesson14Exercise from "./exercises/lesson14/Lesson14Exercise.jsx";
 import RestaurantView from "./views/RestaurantView.jsx";
 
 function App() {
-  return <Lesson14Example />;
-  // return <Lesson14Exercise />;
-  // return <RestaurantView />;
+  return <RestaurantView />;
 }
 
 // Wrap App in an ErrorBoundary to help us with development bugs
--- src/views/RestaurantView.jsx
+++ src/views/RestaurantView.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
+import { useDebouncedCallback } from 'use-debounce';
 import Cart from "../components/Cart/Cart.jsx";
 import MenuItem from "../components/MenuItem/MenuItem.jsx";
-import rawDishes from "../mocks/dishes.json";
 
 import styles from "./RestaurantView.module.css";
 import NavBar from "../components/NavBar/NavBar.jsx";
@@ -13,23 +13,6 @@ const RestaurantView = () => {
   const [dishes, setDishes] = useState([]);
   const [selectedItems, setSelectedItems] = useState([]);
   const [search, setSearch] = useState("a");
-
-  const fetchDishes = async () => {
-    try {
-      const response = await fetch(
-        `https://www.themealdb.com/api/json/v1/1/search.php?f=${search}`
-      );
-      console.log("response :>> ", response);
-      if (!response.ok) {
-        throw new Error("Something bad happened");
-      }
-      const result = await response.json();
-      console.log("result :>> ", result.meals);
-      setDishes(result.meals);
-    } catch (error) {
-      console.log("error :>> ", error);
-    }
-  };
 
   const handleMenuClick = (name) => {
     let nextItems;
@@ -43,17 +26,51 @@ const RestaurantView = () => {
     setSelectedItems(nextItems);
   };
 
-  const handleDishesFilter = (query) => {
-    const filteredDishes = rawDishes.filter((dish) =>
-      dish.name.toLowerCase().includes(query.toLowerCase())
-    );
-
-    setDishes(filteredDishes);
+  const handleDishesFilter = (searchQuery) => {
+    setSearch(searchQuery);
   };
 
-  useEffect(() => {
-    fetchDishes();
-  }, []);
+  // useDebouncedCallback takes a function as a parameter and as the second parameter
+  // the number of milliseconds it should wait until it is actually called so a user
+  // can type freely and as long as they are typing a letter quicker than 500ms, the function won't fire yet.
+  // This is to optimize user experience and communication with the server
+  const debouncedEffectHook = useDebouncedCallback(() => {
+    let currentEffect = true;
+    fetch(
+      `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
+    ).then(res => {
+      if (!res.ok) {
+        return { meals: null };
+      }
+      return res.json();
+    }).then(result => {
+      if (!currentEffect) {
+        return;
+      }
+      // The ?? operator turns 'undefined' or 'null' values into a preferred default value on the right side
+      // We know that result.meals can be null if there are no results, so in that case, we provide an empty array for safety
+      setDishes(result.meals ?? []);
+    }).catch(() => {
+      if (!currentEffect) {
+        return;
+      }
+      setDishes([]);
+    })
+
+    // This cleanup function is to prevent multiple API calls coming back out of sequence and setting the value of our dishes list.
+    // Example:
+    // 1. First search query is pizza -> Network request takes 5 seconds to fetch data
+    // 2. In the meantime, the user types burger instead -> Network request takes 1 second and shows burgers in the list
+    // 3. Then, finally, the first data fetch comes back and overwrites the results, so the search box shows 'burger'
+    //    but the results show pizzas. This is called "stale data"
+    return () => {
+      currentEffect = false;
+    }
+  }, 500);
+
+  // useEffect can take a variable that is a function and does not need to be defined as an anonymous () => {} arrow function
+  // This is especially important when using more controlled techniques like debouncing
+  useEffect(debouncedEffectHook, [debouncedEffectHook, search]);
 
   return (
     <>
