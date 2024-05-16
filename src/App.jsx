e9abd342cb85bd0cf9d0aa4921758de1be59bb84
Fábio Rodrigues <me@fabiorodrigu.es>
2024-05-16 15:26:24 +0200

    feat(Lesson15): added the lesson files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import "./App.css";
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary.jsx";
-import RestaurantView from "./views/RestaurantView.jsx";
+import Lesson15Examples from "./examples/lesson15/Lesson15Examples.jsx";
+// import Lesson15exercise from "./exercises/lesson15/Lesson15Exercises.jsx";
+// import RestaurantView from "./views/RestaurantView.jsx";
 
 function App() {
-  return <RestaurantView />;
+  return <Lesson15Examples />;
+  // return <Lesson15exercise />;
+  // return <RestaurantView />;
 }
 
 // Wrap App in an ErrorBoundary to help us with development bugs
 
-export default function WrappedApp(){
+export default function WrappedApp() {
   return import.meta.env.MODE === "development" ? (
     <ErrorBoundary>
       <App />
---
+++ src/exercises/lesson15/Lesson15Exercises.jsx
@@ -0,0 +1,70 @@
+import styles from "./Lesson15Examples.module.css";
+
+/**
+ * Now it's your turn, make the form below a controlled form.
+ */
+
+const Lesson15exercise = () => {
+  const fullWidthStyles = [styles.inputWrapper, styles.fullWidth].join(" ");
+
+  const handleSubmit = (event) => {
+    // This function is used to prevent the default form submission behavior
+    event.preventDefault();
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <h1>Contact form</h1>
+
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div className={styles.inputWrapper}>
+          <label htmlFor="name">Name:</label>
+
+          <input type="text" id="name" name="name" />
+        </div>
+
+        <div className={styles.inputWrapper}>
+          <label htmlFor="email">Email:</label>
+
+          <input type="email" id="email" name="email" />
+        </div>
+
+        <div className={styles.inputWrapper}>
+          <label htmlFor="country">Country:</label>
+
+          <select id="country" name="country">
+            <option value="USA">USA</option>
+            <option value="Canada">Canada</option>
+            <option value="Mexico">Mexico</option>
+          </select>
+        </div>
+
+        <div />
+
+        <div className={fullWidthStyles}>
+          <label htmlFor="message">Message:</label>
+
+          <textarea id="message" name="message" rows="4" />
+        </div>
+
+        <div className={fullWidthStyles}>
+          <legend>Do you agree to the terms?</legend>
+          <label>
+            <input type="radio" name="terms" value="yes" /> Yes
+          </label>
+          <label>
+            <input type="radio" name="terms" value="no" /> No
+          </label>
+        </div>
+
+        <div className={fullWidthStyles}>
+          <button className={styles.button} type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Lesson15exercise;
